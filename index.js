ba99b7f1cb229a6077cd296d972a437f3776fd12
PRANETH <pranethcableguy@gmail.com>
2024-12-11 11:04:23 +0530

    ADDING A UI OVERHAUL WITH NEW NAVBAR

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 (function(){
-    const quill = new Quill("#editor", {
-      modules: {
-        toolbar: "#toolbar",
-      },
-      theme: "snow",
-    });
+  // const quill = new Quill("#editor", {
+  //   modules: {
+  //     toolbar: "#toolbar",
+  //   },
+  //   theme: "snow",
+  // });
+  const quill = new Quill('#editor', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block'],
+      ],
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow', // or 'bubble'
+  });
 }())
 
